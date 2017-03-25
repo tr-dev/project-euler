e8568a4e405b928a37088f5883132f6a3df61a40
tr mcalexander <tr@mcalexander.me>
2017-03-25 08:39:57 -0400

    Making things "classy"

--- problem-1.js
+++ problem-1.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const Helper = require('./lib/HelperClass');
 /*
 # Multiples of 3 and 5
 - If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
@@ -8,14 +8,25 @@
 - - https://projecteuler.net/problem=1
 */
 //Set our max number
-const UPPER = 1000;
-//The factors we are checking
-const factors = [3, 5];
 
-let sum = 0;
+class Problem1 extends Helper {
+  constructor(limit) {
+    super(1, limit)
 
-for (let counter = UPPER - 1; counter > 0; counter--) {
-  //If we have a factor, include it in our sum, else add nothing
-  sum += (counter % factors[0] === 0 || counter % factors[1] === 0) ? counter : 0;
+  }
+  solve(UPPER) {
+    //The factors we are checking
+    const factors = [3, 5];
+
+    let sum = 0;
+
+    for (let counter = UPPER - 1; counter > 0; counter--) {
+      //If we have a factor, include it in our sum, else add nothing
+      sum += (counter % factors[0] === 0 || counter % factors[1] === 0) ? counter : 0;
+    }
+
+    return sum;
+  }
 }
-console.log(`The sum of the factors is ${sum}`)
+
+(new Problem1(1000)).solution();
--- problem-3.js
+++ problem-3.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const Helper = require('./lib/HelperClass');
 /*
 # Largest prime factor
 - The prime factors of 13195 are 5, 7, 13 and 29.
@@ -8,35 +8,40 @@
 */
 //Set our max number
 const UPPER_LIMIT = 600851475143;
-// const UPPER_LIMIT = 105;
 
+class Problem3 extends Helper {
+  constructor(limit) {
+    super(3, limit);
+  }
+  solve(limit) {
+    //Start from the lowest prime
+    let current = 2;
 
-function findPrime(product) {
-  //Start from the lowest prime
-  let current = 2;
+    //Preserve original limit
+    let product = limit;
+    //Keep track of largest
+    let largest;
 
-  //Keep track of largest
-  let largest;
+    //While are value > 1
+    while (product > 1) {
 
-  //While are value > 1
-  while (product > 1) {
+      //We see if we can divide out the current value
+      if(product % current === 0){
+        //Set product
+        product = product / current;
+        //Track our largest "prime";
+        largest = current;
+        //reset current back to 2
+        current = 2;
+      } else {
+        //Try the next current
+        current++;
+      }
 
-    //We see if we can divide out the current value
-    if(product % current === 0){
-      //Set product
-      product = product / current;
-      //Track our largest "prime";
-      largest = current;
-      //reset current back to 2
-      current = 2;
-    } else {
-      //Try the next current
-      current++;
     }
 
+    return largest;
   }
-
-  return largest;
 }
 
-console.log(`The largest prime factor of ${UPPER_LIMIT} is ${findPrime(UPPER_LIMIT)}`)
+(new Problem3(600851475143)).solution();
